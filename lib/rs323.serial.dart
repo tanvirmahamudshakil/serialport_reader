8ee13f29fa6c5664363f9d8e78a53ff6de32c494
tanviryuma <tanvir.yuma@gmail.com>
2025-01-06 17:14:53 +0600

    update

--- lib/rs323.serial.dart
+++ lib/rs323.serial.dart
@@ -259,6 +259,7 @@ class _RS323SerialPageState extends State<RS323SerialPage> {
                 button(
                     name: "Open",
                     onPress: () {
+                      print("baundRate ${selectedBaudRate}");
                       serialCommunication.openPort(dataFormat: format, serialPort: selectedPort, baudRate: selectedBaudRate);
                     }),
                 button(
